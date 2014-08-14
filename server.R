58413fdfcd8fd6d243bb4ec095e8dee91e2701dc
miguel picallo <mpicallocruz@gmail.com>
2014-08-14 12:06:11 +0200

    Update server.R

--- server.R
+++ server.R
@@ -32,11 +32,4 @@ shinyServer(
   }
 )
 
-##################################
-
-# Documentation:
-  
-# This app computes the body mass index (BMI) for a given person. Given the values for height and weight, 
-# it determines the BMI and concludes in what category is that person. Height can either be given in 
-# centimeters or feet/inches units, and weight in kilograms or pounds.
 
